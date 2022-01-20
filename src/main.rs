dd857cd0b7d85e4363e438537c564357d04f4214
Zhenbo Li <zhenbo.li@uwaterloo.ca>
2022-01-20 15:21:56 +0800

    create rust project as compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let source = args.last().unwrap();
+
+    let source_code = fs::read_to_string(source)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", source_code);
 }
