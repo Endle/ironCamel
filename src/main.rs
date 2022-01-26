38f4c713166ea890c2ace3734bffbd072b55aae8
Zhenbo Li <zhenbo.li@uwaterloo.ca>
2022-01-26 19:41:32 +0800

    Create tokenizer structure

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let source = args.last().unwrap();
+struct ArgConfig {
+    source_code_path: String,
+}
 
-    let source_code = fs::read_to_string(source)
+fn main() {
+    let config = parse_env_args();
+
+    // TODO error check
+    let source_code = fs::read_to_string(&config.source_code_path)
         .expect("Something went wrong reading the file");
 
+    let token_stream = ironcamel::tokenizer::convert_source_to_tokens(&source_code);
     println!("With text:\n{}", source_code);
+}
+
+fn parse_env_args() -> ArgConfig {
+    let args: Vec<String> = env::args().collect();
+    // TODO error check
+    let source = args.last().unwrap();
+    ArgConfig {
+        source_code_path:source.clone()
+    }
 }
---
+++ src/tokenizer.rs
@@ -0,0 +1,10 @@
+pub enum Token {
+
+}
+
+
+pub fn convert_source_to_tokens(code: &str) -> Vec<Token> {
+    let mut result = Vec::new();
+
+    result
+}
