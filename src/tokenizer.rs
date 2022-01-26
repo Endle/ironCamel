786bbf92fcdb15e7f2f4b5ba519618b97a3caf03
Zhenbo Li <zhenbo.li@uwaterloo.ca>
2022-01-26 20:16:19 +0800

    read_next_bracket

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,10 +1,46 @@
 pub enum Token {
-
+    //Bracket
+    LeftParentheses,
+    RightParentheses,
+    LeftSquareBracket,
+    RightSquareBracket,
+    LeftCurlyBracket,
+    RightCurlyBracket,
 }
-
+use crate::tokenizer::Token::*;
 
 pub fn convert_source_to_tokens(code: &str) -> Vec<Token> {
     let mut result = Vec::new();
-
+    let mut pos = 0;
+    while pos < code.len() {
+        let (len, token) = read_next_token(code, pos);
+        pos += len;
+        result.push(token);
+    }
     result
+}
+
+// Return: length of the token, the token
+fn read_next_token(code: &str, pos: usize) -> (usize, Token) {
+    assert!(pos < code.len());
+
+    match read_next_bracket(code[pos]) {
+         None => (),
+         Some(e) => return (1, e),
+    };
+    todo!()
+}
+
+
+fn read_next_bracket(c:char) -> Option<Token> {
+    match c {
+        '{' => Some(LeftCurlyBracket),
+        '}' => Some(RightParentheses),
+        '(' => Some(LeftParentheses),
+        ')' => Some(RightParentheses),
+        '[' => Some(LeftSquareBracket),
+        ']' => Some(RightSquareBracket),
+        _ => None,
+    }
+
 }
