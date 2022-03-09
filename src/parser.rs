9cc9e99705e3201a60e80dc6a2fff89b190125d1
Zhenbo Li <zhenbo.li@uwaterloo.ca>
2022-03-08 19:10:46 -0500

    create ast stub
    Signed-off-by: Zhenbo Li <zhenbo.li@uwaterloo.ca>

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,19 @@
 use log::{warn};
 use crate::tokenizer::Token;
 
-pub fn build_ast(tokens: &Vec<Token>) {
+pub trait AST {
+}
+
+pub struct ProgramAST {
+
+}
+
+impl AST for ProgramAST {
+
+}
+
+pub fn build_ast(tokens: &Vec<Token>) -> ProgramAST {
     warn!("Building ast");
+    let mut result = ProgramAST{};
+    result
 }
