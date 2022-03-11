1ce6b3f392107afa6ca0710ba3834360e896f93e
Zhenbo Li <zhenbo.li@uwaterloo.ca>
2022-03-11 09:03:55 -0500

    Execute on Windows
    Signed-off-by: Zhenbo Li <zhenbo.li@uwaterloo.ca>

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 set -e
 
 cargo build
-RUST_BACKTRACE=1 RUST_LOG=info  target/debug/ironcamel resource/example.icml
+#RUST_BACKTRACE=1 RUST_LOG=info  target/debug/ironcamel resource/example.icml
+
+RUST_BACKTRACE=1 RUST_LOG=info  target/debug/ironcamel.exe resource/example.icml
