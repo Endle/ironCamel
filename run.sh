2bda18e7af4b276560a1ee74060b5570ece69960
Zhenbo Li <lizhenbo@yahoo.com>
2022-04-05 08:33:12 -0400

    Arrange icml files

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ echo "=========== START A NEW BUILD ======"
 cargo build
 #RUST_BACKTRACE=1 RUST_LOG=info  target/debug/ironcamel resource/example.icml
 
-RUST_BACKTRACE=1 RUST_LOG=info  target/debug/ironcamel.exe --include resource/core.icml -i nosuchfile.icml \
---run resource/cons.icml
+RUST_BACKTRACE=1 RUST_LOG=info  target/debug/ironcamel.exe --include include/core.icml -i nosuchfile.icml \
+--run example/fac.icml
