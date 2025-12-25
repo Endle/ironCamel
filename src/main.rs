5f2418b5063492b1b561bae3f136c96dd5db46b5
Zhenbo Li <lizhenbo@yahoo.com>
2025-12-25 10:29:07 -0500

    Create two run mode
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,41 @@ use std::io::Write;
 use clap::Parser;
 
 
+enum RunMode {
+    AdHoc,
+    CompileToLLVMIR,
+}
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// The source file to be executed
-    #[clap(long)]
-    run: String,
+    #[clap(short, long)]
+    run: Option<String>,
+
+    #[clap(short,long)]
+    compile: Option<String>,
 
     /// Libaraies to be included
     #[clap(short, long)]
     include: Vec<String>,
+}
+
+fn read_source_code(args: &Args)->(RunMode, String){
+    if (!args.run.is_none())  &&  (!args.compile.is_none()) {
+        panic!("We can't define both --run and --compile");
+    }
+    if !args.run.is_none() {
+        let main_code = fs::read_to_string(args.run.as_ref().unwrap())
+            .expect("Something went wrong reading the file");
+        return (RunMode::AdHoc, main_code);
+    }
+    if !args.compile.is_none() {
+        let main_code = fs::read_to_string(args.compile.as_ref().unwrap())
+            .expect("Something went wrong reading the file");
+        return (RunMode::CompileToLLVMIR, main_code);
+    }
+    panic!("No source code is provided");
 }
 
 fn main() {
@@ -38,29 +62,34 @@ fn main() {
     info!("Args {:?}", &args);
 
     let mut source_vec = Vec::with_capacity(args.include.len() + 1);
-    for lib_path in args.include {
+    for lib_path in &args.include {
         match fs::read_to_string(&lib_path) {
             Ok(s) => { source_vec.push(s); }
             Err(e) => { error!("Read lib {} failed: {}, skipping\n", lib_path, e) }
         }
     }
-    let main_code = fs::read_to_string(&args.run)
-        .expect("Something went wrong reading the file");
+    let (run_mode, main_code) = read_source_code(&args);
     source_vec.push(main_code);
     let source_code = source_vec.join("\n");
 
     debug!("Source code:\n{}", &source_code);
 
-     let token_stream = ironcamel::tokenizer::convert_source_to_tokens(&source_code);
-     info!("{:?}", &token_stream);
+    let token_stream = ironcamel::tokenizer::convert_source_to_tokens(&source_code);
+    info!("{:?}", &token_stream);
 
-     let ast = ironcamel::parser::build_ast(&token_stream);
-     info!("{:?}", &ast);
-     let ast = pipeline::tree_transform(ast);
-     debug!("{:?}", &ast);
+    let ast = ironcamel::parser::build_ast(&token_stream);
+    info!("{:?}", &ast);
+    let ast = pipeline::tree_transform(ast);
+    debug!("{:?}", &ast);
 
-     ironcamel::interpreter::eval(&ast);
-     // println!("With text:\n{}", source_code);
+    match run_mode {
+        RunMode::AdHoc => {
+            ironcamel::interpreter::eval(&ast);
+        },
+        RunMode::CompileToLLVMIR => {
+            info!("to compile to llvm IR");
+        },
+    }
 
 
 }
