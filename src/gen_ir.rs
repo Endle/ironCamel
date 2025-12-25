4d0f8768049c692f40c8540d0dbb1d1c37290c1f
Zhenbo Li <lizhenbo@yahoo.com>
2025-12-25 15:42:21 -0500

    Use llvm21
    Signed-off-by: Zhenbo Li <lizhenbo@yahoo.com>

--- src/gen_ir.rs
+++ src/gen_ir.rs
@@ -3,9 +3,22 @@
 use crate::parser::{ProgramAST, FunctionAST, BlockAST};
 use log::{debug, info};
 
+use inkwell::context::Context;
 
 
 fn compile_fn(fnast: &FunctionAST){
+    let context = Context::create();
+    let module = context.create_module("ret");
+    let builder = context.create_builder();
+    let i32_type = context.i32_type();
+    let arg_types = [i32_type.into()];
+    let fn_type = i32_type.fn_type(&arg_types, false);
+    let fn_value = module.add_function("ret", fn_type, None);
+    let entry = context.append_basic_block(fn_value, "entry");
+    let i32_arg = fn_value.get_first_param().unwrap();
+
+builder.position_at_end(entry);
+builder.build_return(Some(&i32_arg)).unwrap();
 }
 
 pub fn compile(ast: &ProgramAST) -> String {
